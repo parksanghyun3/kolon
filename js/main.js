c9f93f71bcb48343427b9ce6b073c4065c794af4
easymedia <easymedia@easymedias-MacBook-Air.local>
2022-08-29 08:10:00 +0900

    first commit

--- js/main.js
+++ js/main.js
@@ -1,47 +1,65 @@
 $(document).ready(function(){
   // AOS.init();
 
+  var fixedTop = $(".fixed-con").offset().top;
+
+  
+  $(window).on("scroll", function(){
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop >= fixedTop){
+    }
+  });
+
   gsap.to(".right-layout", {
     ease: "none", 
     scrollTrigger: {
-      trigger: ".fixed-con",
-      start:"top 10%",
-      end: "center top",
+      trigger: ".right-layout",
+      start: "top " + "10%",
+      end: "bottom bottom",
       // markers: true,
-      pin: ".right-layout",
-      // pinSpacing: false,
+      pin: true,
+      pinSpacing: false,
       scrub: true,
     }
   });
 
-  
-  $(".fixed-img").each(function(i){
-    gsap.to($(".fixed-img").eq(i), {
-      top: "-100%",
-      stagger: .5,
-      scrollTrigger: {
-        trigger: $(".left-layout").eq(i),
-        start: "12% top",
-        end: "90% top",
-        scrub: true,
-        markers: true,
-      },
-    })
-  })
+  $(".txt-list").each(function(){
 
-  // gsap.to(".fixed-img", {
-  //   top: "-100%",
-  //   stagger: .5,
-  //   scrollTrigger: {
-  //     trigger: "#section1",
-  //     start: "12% top",
-  //     end: "90% top",
-  //     // markers: true,
-  //     pin: ".fixed-img",
-  //     pinSpacing: false,
-  //     scrub: true,
-  //   }
+  });
+  gsap.to(".fixed-img", {
+    top: "-100%",
+    stagger: .5,
+    scrollTrigger: {
+      trigger: ".txt-list",
+      start: "top top",
+      end: "bottom bottom",
+      pin: true,
+      markers: true,
+      pinSpacing: false,
+      scrub: true,
+    }
+  });
+
+  // gsap.set(".right-layout", {
+  //   ease: "none", 
+  //   top: "50%",
+  //   yPercent: "-50%",
   // });
+
+  
+  // $(".fixed-img").each(function(i){
+  //   gsap.to($(".fixed-img").eq(i), {
+  //     top: "-100%",
+  //     stagger: .5,
+  //     scrollTrigger: {
+  //       trigger: $(".left-layout").eq(i),
+  //       start: "12% top",
+  //       end: "90% top",
+  //       scrub: true,
+  //       markers: true,
+  //     },
+  //   })
+  // })
 
 
   // gsap.to(".txt-list", {
